0f0536e317b1124f462586a00b0a5b200a0a581f
Eduardo <feduardo191@gmail.com>
2020-08-21 13:46:58 -0300

    Estruturando Layout da página

--- src/components/LeftColumn/ProfilePanel/index.tsx
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -10,22 +10,22 @@ const ProfilePanel: React.FC = () => {
       <Container>
         <div className="profile-cover"></div>
         <img
-          src="https://github.com/guilhermerodz.png"
+          src="https://github.com/Eduardo191.png"
           alt="Avatar"
           className="profile-picture"
         />
-        <h1>Guilherme Rodz</h1>
-        <h2>Software Engineer @ Rocketseat</h2>
+        <h1>Eduardo Ferreira</h1>
+        <h2>Estudante de programação</h2>
 
         <div className="separator"></div>
 
         <div className="key-value">
           <span className="key">Quem viu seu perfil</span>
-          <span className="value">1.558</span>
+          <span className="value">34</span>
         </div>
         <div className="key-value">
           <span className="key">Viram sua publicação</span>
-          <span className="value">388</span>
+          <span className="value">12</span>
         </div>
       </Container>
     </Panel>
